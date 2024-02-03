cc84aa6da672f66de0a1f96db9ec3fe0e6e115ce
Jonathan Cardoso <jonathan99mour@gmail.com>
2024-02-03 18:42:53 -0300

    feat: add url to category charts and functions utils ti get infos from companies

--- headcount/urls.py
+++ headcount/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HeadcountInPeriod
+from .views import HeadcountInPeriod, HeadcountInLastPeriod
 
 urlpatterns = [
     path('line_chart/', HeadcountInPeriod.as_view(), name='HeadcountInPeriod-line-chart'),
+    path('category_charts/', HeadcountInLastPeriod.as_view(), name='HeadcountInPeriod-graph-chart'),
 ]
--- headcount/utils.py
+++ headcount/utils.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
+import calendar
 
 
-def generate_month_array(init_date, end_date):
+def generate_months(init_date, end_date):
     init_date_obj = datetime.strptime(init_date, '%Y-%m-%d')
     end_date_obj = datetime.strptime(end_date, '%Y-%m-%d')
 
@@ -14,7 +15,7 @@ def generate_month_array(init_date, end_date):
     
     return months
 
-def generate_series_array(init_date, months, monthly_employee_count):
+def generate_series(init_date, months, monthly_employee_count):
     current_year = datetime.strptime(init_date, '%Y-%m-%d').year
     employee_in_year = [
         monthly_employee_count[i:i + 12] for i in range(0, len(monthly_employee_count), 12)
@@ -36,3 +37,28 @@ def generate_series_array(init_date, months, monthly_employee_count):
             current_data.append(employee_in_year[len(series) - 1])
 
     return series
+
+def get_first_and_last_day(date_str):
+    input_date = datetime.strptime(date_str, '%Y-%m-%d')
+
+    first_day_of_month = input_date.replace(day=1)
+
+    _, last_day_of_month = calendar.monthrange(first_day_of_month.year, first_day_of_month.month)
+    last_day_of_month = first_day_of_month.replace(day=last_day_of_month)
+
+    formatted_first_day = first_day_of_month.strftime('%Y-%m-%d')
+    formatted_last_day = last_day_of_month.strftime('%Y-%m-%d')
+
+    return formatted_first_day, formatted_last_day
+
+def get_infos_from_company(employees):
+    companies_name = list(employees.values_list('ds_category_1', flat=True).distinct())
+
+    company_counts = {}
+    for employee in employees:
+        company_name = employee.ds_category_1
+        company_counts[company_name] = company_counts.get(company_name, 0) + 1
+
+    employee_counts_by_company = [count for _, count in company_counts.items()]
+
+    return companies_name, employee_counts_by_company
